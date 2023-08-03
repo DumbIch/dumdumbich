ccdb457665f8bc2d2bf0a1aaae21a1d32cbb7fba
dumbich <dumdumbich@mail.ru>
2023-08-03 08:26:17 +0300

    Custom model manager

--- dumdumbich/blog/models.py
+++ dumdumbich/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -27,6 +32,9 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT,
     )
+
+    object = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
